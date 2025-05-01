2286ba157e7cf063105362738d124d37b6e5f20e
Joe Mock <jmock@zagmail.gonzaga.edu>
2025-05-01 12:56:30 -0700

    Added new constructors, going to try some implementations, think this will make ease of use much easier

--- RiverTree.cpp
+++ RiverTree.cpp
@@ -2,24 +2,50 @@
 #include <iostream>
 
 // Default constructor, normally should never be used
+Dam::Dam(){
+    name;
+}
+Dam::Dam(char name[]){
+    for(int i = 0; i < 99; ++i){
+        this->name[i] = name[i];     // need to make sure name is filled with null after meaningful character
+    }
+}
+Tributary::Tributary(){
+    name;
+    length = 0;
+    basinSize = 0;
+    averageDischarge = 0;
+}
+Tributary::Tributary(char name[100], int length, int basinSize, int averageDischarge){
+    for(int i = 0; i < 100; ++i){
+        this->name[i] = name[i];    // need to make sure name is filled with null after meaningful character
+    }
+    this->length = length;
+    this->basinSize = basinSize;
+    this->averageDischarge = averageDischarge;
+}
 RiverNode::RiverNode(){
-    this->type = "";
-    this->length = -1;
+    this->dam = new Dam();
+    this->trib = new Tributary();
     this->left = nullptr;
     this->right = nullptr;
 }
-// Dam constructor, type is assumed to be dam, name can be the name of a dam
-RiverNode::RiverNode(std::string name){
-    this->type = "Dam"   ;   // "Dam"
-    this->length = 0;             // Dam has no length, tributary does I think
-    this->left = nullptr;
-    this->right = nullptr;
+// RIVERNODE Constructor, type is 1 for tributary node, 2 is dam node
+RiverNode::RiverNode(int type){
+    if(type == 1){
+        this->trib = new Tributary();
+        this->dam = nullptr;
+        this->left = nullptr;
+        this->right = nullptr;
+    }else if(type == 2){
+        this->dam = new Dam();
+        this->trib = nullptr;
+        this->left = nullptr;
+        this->right = nullptr;
+
+    }else{
+        RiverNode();
+    }
+    
 }
-// Tributary constructor, type is assumed to be tributary becayse dams do not ahve a length
-RiverNode::RiverNode(std::string name, int length){
-    this->type = "Tributary";     // "Tributary"
-    this->name = name;      // name of trib
-    this->length = length;  // length of trib
-    this->left = nullptr;
-    this->right = nullptr;
-}
+
